2c9cacf0faef4c5dca34333ed0119379b1b4b499
Alejandro Alzate <alejoalzate12@gmail.com>
2024-03-27 23:03:15 -0500

    Add product class in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,26 @@ void main() {
   runApp(const MyApp());
 }
 
+class Product {
+  final int id;
+  final String name;
+  final String description;
+  final double price;
+  final int stock;
+  final String brand;
+  final String supplier;
+
+  Product({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.price,
+    required this.stock,
+    required this.brand,
+    required this.supplier,
+  });
+}
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
